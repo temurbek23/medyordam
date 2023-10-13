d0d935b5fcd2b2dc1e5fac28fc50b0a3abb811d1
unknown <ibn970sina@gmail.com>
2023-10-13 17:18:41 +0500

    making first controllers and migratioins

--- database/migrations/2023_10_13_120948_create_patients_table.php
+++ database/migrations/2023_10_13_120948_create_patients_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('patients', function (Blueprint $table) {
             $table->id();
             $table->string('firstname');
             $table->string('lastname');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('patients');
     }
 };
